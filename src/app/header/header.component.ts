ed8357041b74584f1858c7969a8e53b9d2dc390a
Hashan Munasinghe <hashan.nilupul@gmail.com>
2024-08-21 22:27:16 +0530

    implemented mobile responsive nav bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isMobileMenuActive: boolean = false;
   
+  toggleMobileMenu(): void {
+    this.isMobileMenuActive = !this.isMobileMenuActive;
+  }
 }
