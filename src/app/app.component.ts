1fee4eb9b7f59c455f158899383f538abe07fa1b
Hashan Munasinghe <hashan.nilupul@gmail.com>
2024-09-01 23:43:54 +0530

    routing configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  template: `
+    <app-header></app-header>
+    <main class="body-container">
+      <div class="body-inner">
+        <router-outlet></router-outlet>
+      </div>
+    </main>
+    <app-footer></app-footer>
+  `,
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, HomeComponent],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Hashan Nilupul | UX Designer',
+        component: HomeComponent,
+    },
+    {
+        path: 'projects',
+        title: 'Hashan Nilupul | Projects',
+        component: ProjectsComponent,
+    },
+    {
+        path: 'about-me',
+        title: 'Hashan Nilupul | About me',
+        component: AboutMeComponent,
+    }
+
+];
