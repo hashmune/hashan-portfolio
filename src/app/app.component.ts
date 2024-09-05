3798ede626ea23f19e9aadb12e4a40c395e74483
Hashan Munasinghe <hashan.nilupul@gmail.com>
2024-09-05 21:33:49 +0530

    mobile responsive fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -27,4 +27,30 @@ import { ProjectsComponent } from './projects/projects.component';
   ],
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit, OnDestroy {
+  private mobileMenu: HTMLElement | null = null;
+  private mobileMenuObserver: MutationObserver;
+
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {
+    this.mobileMenuObserver = new MutationObserver(() => this.checkMenuStatus());
+  }
+
+  ngAfterViewInit() {
+    this.mobileMenu = this.elRef.nativeElement.querySelector('.mobile-menu');
+    if (this.mobileMenu) {
+      this.mobileMenuObserver.observe(this.mobileMenu, { attributes: true });
+    }
+  }
+
+  ngOnDestroy() {
+    this.mobileMenuObserver.disconnect();
+  }
+
+  checkMenuStatus() {
+    if (this.mobileMenu?.classList.contains('show-menu')) {
+      this.renderer.addClass(this.elRef.nativeElement, 'overflow-hidden');
+    } else {
+      this.renderer.removeClass(this.elRef.nativeElement, 'overflow-hidden');
+    }
+  }
+}
