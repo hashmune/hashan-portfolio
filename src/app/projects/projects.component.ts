ca53bfc2cc36f772772457f70ed64caf36267d51
Hashan Munasinghe <hashan.nilupul@gmail.com>
2024-09-15 18:10:30 +0530

    new typography mixins

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,8 +6,33 @@ import { ProjectCardComponent } from '../project-card/project-card.component';
   standalone: true,
   imports: [ProjectCardComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      id: 1,
+      imageSrc: 'sample-image.png',
+      title: 'Project 1',
+      description: 'Description for project 1',
+    },
+    {
+      id: 2,
+      imageSrc: 'sample-image.png',
+      title: 'Project 2',
+      description: 'Description for project 2',
+    },
+    {
+      id: 3,
+      imageSrc: 'sample-image.png',
+      title: 'Project 3',
+      description: 'Description for project 3',
+    },
+    {
+      id: 4,
+      imageSrc: 'sample-image.png',
+      title: 'Project 4',
+      description: 'Description for project 4',
+    }
+  ];
 }
