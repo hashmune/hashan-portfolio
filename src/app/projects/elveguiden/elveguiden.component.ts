fdbe2737d617bfac77e58a4578153c86014cc79a
Hashan Munasinghe <hashan.nilupul@gmail.com>
2024-09-16 22:33:07 +0530

    Project details template base

--- src/app/projects/elveguiden/elveguiden.component.ts
+++ src/app/projects/elveguiden/elveguiden.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-elveguiden',
   standalone: true,
-  imports: [],
+  imports: [ ButtonModule ],
   templateUrl: './elveguiden.component.html',
   styleUrl: './elveguiden.component.scss'
 })
 export class ElveguidenComponent {
 
+  constructor (
+    private location: Location
+  ) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
