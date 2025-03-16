9983098dc45953de5464573ffb52ad79761580bc
Hashan Munasinghe <hashan.nilupul@gmail.com>
2025-03-16 18:43:00 +0530

    Temp project details (#13)
    * Project component with empty state
    
    * Added all project temp figma url

--- src/app/projects/elveguiden/elveguiden.component.ts
+++ src/app/projects/elveguiden/elveguiden.component.ts
@@ -5,21 +5,26 @@ import {
   ViewChild,
   Renderer2,
   HostListener,
-} from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { DynamicMarginTopService } from '../../services/dynamic-margin-service/dynamic-margin-top.service';
-import { GoBackNavigationService } from '../../services/go-back/go-back-navigation.service';
+} from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DynamicMarginTopService } from "../../services/dynamic-margin-service/dynamic-margin-top.service";
+import { GoBackNavigationService } from "../../services/go-back/go-back-navigation.service";
+import { DialogModule } from "primeng/dialog";
+import { InputOtpModule } from "primeng/inputotp";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-elveguiden',
+  selector: "app-elveguiden",
   standalone: true,
-  imports: [ButtonModule],
-  templateUrl: './elveguiden.component.html',
-  styleUrl: './elveguiden.component.scss',
+  imports: [ButtonModule, DialogModule, InputOtpModule, FormsModule],
+  templateUrl: "./elveguiden.component.html",
+  styleUrl: "./elveguiden.component.scss",
 })
 export class ElveguidenComponent implements AfterViewInit {
-  @ViewChild('cover') cover!: ElementRef;
-  @ViewChild('details') details!: ElementRef;
+  @ViewChild("cover") cover!: ElementRef;
+  @ViewChild("details") details!: ElementRef;
+
+  visible: boolean = false;
 
   constructor(
     private renderer: Renderer2,
@@ -31,20 +36,20 @@ export class ElveguidenComponent implements AfterViewInit {
     this.updateMargin();
   }
 
-  @HostListener('window:resize')
+  @HostListener("window:resize")
   onResize() {
     this.updateMargin();
   }
 
   updateMargin(): void {
-    this.DynamicMarginTopService.updateMarginTop(
-      this.cover,
-      this.details,
-      this.renderer
-    );
+    this.DynamicMarginTopService.updateMarginTop(this.cover, this.details, this.renderer);
   }
 
   goBack(): void {
     this.GoBackNavigationService.goBack();
+  }
+
+  showDialog() {
+    this.visible = true;
   }
 }
--- src/app/projects/uni-design-system/uni-design-system.component.ts
+++ src/app/projects/uni-design-system/uni-design-system.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  Renderer2,
+  HostListener,
+} from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DynamicMarginTopService } from "../../services/dynamic-margin-service/dynamic-margin-top.service";
+import { GoBackNavigationService } from "../../services/go-back/go-back-navigation.service";
+import { DialogModule } from "primeng/dialog";
+import { InputOtpModule } from "primeng/inputotp";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-uni-design-system',
+  selector: "app-uni-design-system",
   standalone: true,
-  imports: [],
-  templateUrl: './uni-design-system.component.html',
-  styleUrl: './uni-design-system.component.scss'
+  imports: [ButtonModule, DialogModule, InputOtpModule, FormsModule],
+  templateUrl: "./uni-design-system.component.html",
+  styleUrl: "./uni-design-system.component.scss",
 })
-export class UniDesignSystemComponent {
+export class UniDesignSystemComponent implements AfterViewInit {
+  @ViewChild("cover") cover!: ElementRef;
+  @ViewChild("details") details!: ElementRef;
 
+  visible: boolean = false;
+  passwordInput!: string;
+  isPasswordCorrect: boolean = false;
+
+  private correctPassword: string = "1994";
+
+  constructor(
+    private renderer: Renderer2,
+    private DynamicMarginTopService: DynamicMarginTopService,
+    private GoBackNavigationService: GoBackNavigationService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.updateMargin();
+  }
+
+  @HostListener("window:resize")
+  onResize() {
+    this.updateMargin();
+  }
+
+  updateMargin(): void {
+    this.DynamicMarginTopService.updateMarginTop(this.cover, this.details, this.renderer);
+  }
+
+  goBack(): void {
+    this.GoBackNavigationService.goBack();
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  checkPassword(): void {
+    this.isPasswordCorrect = this.passwordInput === this.correctPassword;
+    if (!this.isPasswordCorrect) {
+      alert("Incorrect password. Please try again.");
+      this.passwordInput = "";
+    }
+  }
+
+  modalHide() {
+    this.passwordInput = "";
+    this.isPasswordCorrect = false;
+  }
 }
