110a1364f9a0651ad06af408d9145400c33881c3
Hashan Munasinghe <hashan.nilupul@gmail.com>
2025-03-16 14:56:34 +0530

    Project component with empty state

--- src/app/projects/elveguiden/elveguiden.component.ts
+++ src/app/projects/elveguiden/elveguiden.component.ts
@@ -5,21 +5,30 @@ import {
   ViewChild,
   Renderer2,
   HostListener,
-} from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { DynamicMarginTopService } from '../../services/dynamic-margin-service/dynamic-margin-top.service';
-import { GoBackNavigationService } from '../../services/go-back/go-back-navigation.service';
+} from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DynamicMarginTopService } from "../../services/dynamic-margin-service/dynamic-margin-top.service";
+import { GoBackNavigationService } from "../../services/go-back/go-back-navigation.service";
+import { DialogModule } from "primeng/dialog";
+import { InputOtpModule } from "primeng/inputotp";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-elveguiden',
+  selector: "app-elveguiden",
   standalone: true,
-  imports: [ButtonModule],
-  templateUrl: './elveguiden.component.html',
-  styleUrl: './elveguiden.component.scss',
+  imports: [ButtonModule, DialogModule, InputOtpModule, FormsModule],
+  templateUrl: "./elveguiden.component.html",
+  styleUrl: "./elveguiden.component.scss",
 })
 export class ElveguidenComponent implements AfterViewInit {
-  @ViewChild('cover') cover!: ElementRef;
-  @ViewChild('details') details!: ElementRef;
+  @ViewChild("cover") cover!: ElementRef;
+  @ViewChild("details") details!: ElementRef;
+
+  visible: boolean = false;
+  passwordInput!: string;
+  isPasswordCorrect: boolean = false;
+
+  private correctPassword: string = "1994";
 
   constructor(
     private renderer: Renderer2,
@@ -31,20 +40,33 @@ export class ElveguidenComponent implements AfterViewInit {
     this.updateMargin();
   }
 
-  @HostListener('window:resize')
+  @HostListener("window:resize")
   onResize() {
     this.updateMargin();
   }
 
   updateMargin(): void {
-    this.DynamicMarginTopService.updateMarginTop(
-      this.cover,
-      this.details,
-      this.renderer
-    );
+    this.DynamicMarginTopService.updateMarginTop(this.cover, this.details, this.renderer);
   }
 
   goBack(): void {
     this.GoBackNavigationService.goBack();
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  checkPassword(): void {
+    this.isPasswordCorrect = this.passwordInput === this.correctPassword;
+    if (!this.isPasswordCorrect) {
+      alert("Incorrect password. Please try again.");
+      this.passwordInput = "";
+    }
+  }
+
+  modalHide() {
+    this.passwordInput = "";
+    this.isPasswordCorrect = false;
   }
 }
