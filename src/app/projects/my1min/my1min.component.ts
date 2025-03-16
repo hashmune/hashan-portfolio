8f49837511fe6b8d2f33089c4aef3ba15c80501b
Hashan Munasinghe <hashan.nilupul@gmail.com>
2025-03-16 18:39:47 +0530

    Added all project temp figma url

--- src/app/projects/my1min/my1min.component.ts
+++ src/app/projects/my1min/my1min.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  Renderer2,
+  HostListener,
+} from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DynamicMarginTopService } from "../../services/dynamic-margin-service/dynamic-margin-top.service";
+import { GoBackNavigationService } from "../../services/go-back/go-back-navigation.service";
+import { DialogModule } from "primeng/dialog";
+import { InputOtpModule } from "primeng/inputotp";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-my1min',
+  selector: "app-my1min",
   standalone: true,
-  imports: [],
-  templateUrl: './my1min.component.html',
-  styleUrl: './my1min.component.scss'
+  imports: [ButtonModule, DialogModule, InputOtpModule, FormsModule],
+  templateUrl: "./my1min.component.html",
+  styleUrl: "./my1min.component.scss",
 })
-export class My1minComponent {
+export class My1minComponent implements AfterViewInit {
+  @ViewChild("cover") cover!: ElementRef;
+  @ViewChild("details") details!: ElementRef;
 
+  visible: boolean = false;
+  passwordInput!: string;
+  isPasswordCorrect: boolean = false;
+
+  private correctPassword: string = "1994";
+
+  constructor(
+    private renderer: Renderer2,
+    private DynamicMarginTopService: DynamicMarginTopService,
+    private GoBackNavigationService: GoBackNavigationService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.updateMargin();
+  }
+
+  @HostListener("window:resize")
+  onResize() {
+    this.updateMargin();
+  }
+
+  updateMargin(): void {
+    this.DynamicMarginTopService.updateMarginTop(this.cover, this.details, this.renderer);
+  }
+
+  goBack(): void {
+    this.GoBackNavigationService.goBack();
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  checkPassword(): void {
+    this.isPasswordCorrect = this.passwordInput === this.correctPassword;
+    if (!this.isPasswordCorrect) {
+      alert("Incorrect password. Please try again.");
+      this.passwordInput = "";
+    }
+  }
+
+  modalHide() {
+    this.passwordInput = "";
+    this.isPasswordCorrect = false;
+  }
 }
--- src/app/projects/uni-design-system/uni-design-system.component.spec.ts
+++ src/app/projects/uni-design-system/uni-design-system.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { UniDesignSystemComponent } from './uni-design-system.component';
+import { UniDesignSystemComponent } from "./uni-design-system.component";
 
-describe('UniDesignSystemComponent', () => {
+describe("UniDesignSystemComponent", () => {
   let component: UniDesignSystemComponent;
   let fixture: ComponentFixture<UniDesignSystemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UniDesignSystemComponent]
-    })
-    .compileComponents();
+      imports: [UniDesignSystemComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UniDesignSystemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
